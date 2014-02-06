7ab88dd47315b3c6c0a67a3e57d1f02ed498b802
Gary Zeien <gzeien@us.ibm.com>
2014-02-06 13:39:28 -0600

    Test 1

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ var http = require('http');
 
 http.createServer(function(req,res) {
      res.writeHead(200,{'Content-Type' : 'text/plain'});
-     res.end('Hello World from NodeJS - GZ2');
+     res.end('Hello World from NodeJS - GZ');
 }).listen(port,host);
